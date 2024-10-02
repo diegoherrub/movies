b8d44b5532300557726f374af2ba8e3a311fcb6b
PolRubiano <diego@pigames.es>
2024-10-02 19:59:28 +0200

    241002_extension para el load url y evitar problemas con glide

---
+++ app/src/main/java/edu/iesam/movies/extensions/ImageViewExt.kt
@@ -0,0 +1,15 @@
+package edu.iesam.movies.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+/**
+ * Con esta función nos protegemos para cargar las url de las imágenes
+ * por si en un futuro Glide cambiara (de gratis a pago...)
+ */
+fun ImageView.loadUrl(url: String){
+    Glide
+        .with(this)
+        .load(url)
+        .into(this)
+}
